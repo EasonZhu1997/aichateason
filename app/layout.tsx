ee05b3ffcb6d413105f800a61071d3796d3627b0
YixiaoZhu <2630281769@qq.com>
2025-02-27 13:52:38 +0800

    0.2.2e3
    修改了目录

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "分掌门AI答疑老师",
   description: "分掌门AI答疑老师",
+  icons: {
+    icon: '/fzmlogo.png'
+  }
 };
 
 export default function RootLayout({
